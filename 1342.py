484f78c8755ee0bd443ceaf73e515fa954388a9b
Hao Jia Li <lihaojia@vip.qq.com>
2022-01-31 07:12:20 +0800

    Create 1342.py

---
+++ 1342.py
@@ -0,0 +1,10 @@
+class Solution:
+  def numberOfSteps(self, num: int) -> int:
+    step = 0
+    while num:
+      step += 1
+      if num & 1:
+        num -= 1
+      else:
+        num >>= 1
+    return step
