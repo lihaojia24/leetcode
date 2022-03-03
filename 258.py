382ba0cffe61266dfa567844e2892b4070a42be2
Hao Jia Li <lihaojia@vip.qq.com>
2022-03-03 10:02:33 +0800

    Create 258.py

---
+++ 258.py
@@ -0,0 +1,12 @@
+class Solution:
+  def addDigits(self, num: int) -> int:
+    while num > 9:
+      tmp = 0
+      while num > 0:
+        tmp += num % 10
+        num //= 10
+      num = tmp
+    return num
+
+s = Solution()
+print(s.addDigits(89))
