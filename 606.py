0e497bb0031be1c8aa3ffb4d13eeab44bc18cee3
Hao Jia Li <lihaojia@vip.qq.com>
2022-03-19 12:03:01 +0800

    Create 606.py

---
+++ 606.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+#digui
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+      if root is None:
+        return ""
+      if root.left is None and root.right is None:
+        return f"{root.val}"
+      if root.right is None:
+        return f"{root.val}({self.tree2str(root.left)})"
+      return f"{root.val}({self.tree2str(root.left)})({self.tree2str(root.right)})"
+      
+#diedai
+class Solution:
+  def tree2str(self, root: Optional[TreeNode]) -> str:
