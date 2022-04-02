b381d6da08cbdd1f45c56a520281a415ff5144e6
Hao Jia Li <lihaojia@vip.qq.com>
2022-04-02 10:15:44 +0800

    update

--- 954.py
+++ 954.py
@@ -5,7 +5,7 @@ class Solution:
   def canReorderDoubled(self, arr: List[int]) -> bool:
     cnt = Counter(arr)
     if cnt[0] % 2: return False
-    for x in sorted(arr, key=abs):
-      if arr[2 * x] < arr[x]: return False
-      arr[2 * x] -= arr[x]
+    for x in sorted(cnt, key=abs):
+      if cnt[2 * x] < cnt[x]: return False
+      cnt[2 * x] -= cnt[x]
     return True
