93f5f9e147fb6281ae4b170efe5be5216b152cd7
Hao Jia Li <lihaojia@vip.qq.com>
2022-04-24 21:35:37 +0800

    Create 868.go

---
+++ go/868.go
@@ -0,0 +1,27 @@
+package main
+
+func binaryGap(n int) (ans int) {
+	last := -1
+	for i := 0; n > 0; i++ {
+		if n&1 == 1 {
+			if last != -1 {
+				ans = max(ans, i-last)
+			}
+			last = i
+		}
+		n >>= 1
+	}
+	return
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func main() {
+
+}
