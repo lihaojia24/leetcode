b9f495587ec8993c67bfa4302f3881ac736f3af0
Hao Jia Li <lihaojia@vip.qq.com>
2022-04-25 17:19:05 +0800

    Create 398.go

---
+++ go/398.go
@@ -0,0 +1,34 @@
+package main
+
+import "math/rand"
+
+type Solution struct {
+	nums []int
+}
+
+func Constructor(nums []int) Solution {
+	return Solution{nums}
+}
+
+func (this *Solution) Pick(target int) (ans int) {
+	cnt := 0
+	for i, num := range this.nums {
+		if num == target {
+			cnt++
+			if rand.Intn(cnt) == 0 {
+				ans = i
+			}
+		}
+	}
+	return
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.Pick(target);
+ */
+
+func main() {
+
+}
