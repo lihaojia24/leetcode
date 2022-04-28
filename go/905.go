f3b6bfbdd397a8101228f9c4045f140ff84d9a51
Hao Jia Li <lihaojia@vip.qq.com>
2022-04-28 10:51:53 +0800

    Create 905.go

---
+++ go/905.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func sortArrayByParity(nums []int) []int {
+	i, j := 0, len(nums)-1
+	for i < j {
+		if nums[i]%2 == 1 {
+			nums[i], nums[j] = nums[j], nums[i]
+			j--
+		} else {
+			i++
+		}
+	}
+	return nums
+}
+
+func main() {
+	nums := []int{3, 1, 2, 4}
+	ans := sortArrayByParity(nums)
+	fmt.Printf("ans: %v\n", ans)
+}
