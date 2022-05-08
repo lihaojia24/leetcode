5c9b051fa23e75d4e447299728d1f45017a670b7
Hao Jia Li <lihaojia@vip.qq.com>
2022-05-08 16:58:31 +0800

    Create 442.go

---
+++ go/442.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func findDuplicates(nums []int) (ans []int) {
+	for _, num := range nums {
+		if num < 0 {
+			num = -num
+		}
+		if nums[num-1] < 0 {
+			ans = append(ans, num)
+		} else {
+			nums[num-1] = -nums[num-1]
+		}
+	}
+	return
+}
+
+func main() {
+	nums := []int{1, 1, 2, 3, 3, 4, 5, 2}
+	fmt.Printf("findDuplicates(nums): %v\n", findDuplicates(nums))
+}
