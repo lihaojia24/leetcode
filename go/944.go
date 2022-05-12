b5c81cf41ebdfb8534f546d604e83e8e22437634
Hao Jia Li <lihaojia@vip.qq.com>
2022-05-12 13:46:16 +0800

    Create 944.go

---
+++ go/944.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func minDeletionSize(strs []string) (ans int) {
+	row := len(strs)
+	col := len(strs[0])
+	for c := 0; c < col; c++ {
+		tmp := strs[0][c]
+		for r := 1; r < row; r++ {
+			if strs[r][c] >= tmp {
+				tmp = strs[r][c]
+			} else {
+				ans++
+				break
+			}
+		}
+	}
+	return
+}
+
+func main() {
+	strs := []string{"abc", "bce", "caa"}
+	ans := minDeletionSize(strs)
+	fmt.Printf("ans: %v\n", ans)
+}
