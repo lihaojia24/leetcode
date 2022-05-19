93cb3d3cc03ad52ebef83e354f9704f4af66707a
Hao Jia Li <lihaojia@vip.qq.com>
2022-05-19 09:47:10 +0800

    Create 462.go

---
+++ go/462.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func minMoves2(nums []int) (ans int) {
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+	left, right := 0, len(nums)-1
+	for left < right {
+		ans += nums[right] - nums[left]
+		left++
+		right--
+	}
+	return
+}
+
+func main() {
+	fmt.Printf("minMoves2([]int{1, 10, 2, 9}): %v\n", minMoves2([]int{1, 10, 2, 9}))
+}
