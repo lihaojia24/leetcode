79a18229273cf1e06d75a2e74f910bee79020140
Hao Jia Li <lihaojia@vip.qq.com>
2022-05-28 12:07:47 +0800

    Create 1021.go

---
+++ go/1021.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func removeOuterParentheses(s string) string {
+	leftCnt := 0
+	ans := []rune{}
+	for _, ch := range s {
+		// fmt.Printf("ch: %v\n", ch)
+		if ch == '(' {
+			leftCnt++
+			if leftCnt == 1 {
+				continue
+			}
+		}
+		if ch == ')' {
+			leftCnt--
+			if leftCnt == 0 {
+				continue
+			}
+		}
+		ans = append(ans, ch)
+	}
+	return string(ans)
+
+}
+
+func main() {
+	s := "(()())(())(()(()))"
+	fmt.Printf("removeOuterParentheses(s): %v\n", removeOuterParentheses(s))
+}
