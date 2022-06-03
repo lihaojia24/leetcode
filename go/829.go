bb725b05dc283ce285d22f2260dc6943def11819
Hao Jia Li <lihaojia@vip.qq.com>
2022-06-03 10:27:45 +0800

    Create 829.go

---
+++ go/829.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func consecutiveNumbersSum(n int) int {
+	// (s + s + k - 1) * k = 2 * n
+	// (2s + k - 1) * k = 2n
+	n2 := 2 * n
+	ans := 0
+	for k := 1; k*k <= n2; k++ {
+		if n2%k == 0 {
+			a := n2/k - k + 1
+			if a > 0 && a%2 == 0 {
+				ans++
+			}
+		}
+	}
+	return ans
+}
+
+func main() {
+	fmt.Printf("consecutiveNumbersSum(5): %v\n", consecutiveNumbersSum(15))
+}
