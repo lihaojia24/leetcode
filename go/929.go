9d7ee3b6ffa1a5f433218eb242a19afc215988d6
Hao Jia Li <lihaojia@vip.qq.com>
2022-06-04 21:36:56 +0800

    Create 929.go

---
+++ go/929.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"strings"
+)
+
+func numUniqueEmails(emails []string) int {
+	ans := map[string]struct{}{}
+	for _, email := range emails {
+		field := strings.Split(email, "@")
+		local_name, host_name := field[0], field[1]
+		local_name = strings.Split(local_name, "+")[0]
+		local_name = strings.ReplaceAll(local_name, ".", "")
+		ans[local_name+"@"+host_name] = struct{}{}
+	}
+	return len(ans)
+}
+
+func main() {
+
+}
