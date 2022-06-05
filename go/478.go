4e5b9feb48e1bb21809a36ce75c3c08df6a7976c
Hao Jia Li <lihaojia@vip.qq.com>
2022-06-05 18:35:08 +0800

    Create 478.go

---
+++ go/478.go
@@ -0,0 +1,31 @@
+package main
+
+import "math/rand"
+
+type Solution struct {
+	radius, x_center, y_center float64
+}
+
+func Constructor(radius float64, x_center float64, y_center float64) Solution {
+	return Solution{radius, x_center, y_center}
+}
+
+func (this *Solution) RandPoint() []float64 {
+	for {
+		x := rand.Float64()*2 - 1
+		y := rand.Float64()*2 - 1
+		if x*x+y*y < 1 {
+			return []float64{x*this.radius + this.x_center, y*this.radius + this.y_center}
+		}
+	}
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(radius, x_center, y_center);
+ * param_1 := obj.RandPoint();
+ */
+
+func main() {
+
+}
