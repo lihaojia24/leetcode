9742eb39d6e15615e84dc9dd5f09f8a67330c2b0
Hao Jia Li <lihaojia@vip.qq.com>
2022-06-13 08:27:52 +0800

    Update 926.go

--- go/926.go
+++ go/926.go
@@ -2,18 +2,25 @@ package main
 
 import "fmt"
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
 func minFlipsMonoIncr(s string) int {
-	num, flag := 0, 0
+	num0, num1 := 0, 0
 	for _, ch := range s {
 		if ch == '1' {
-			flag = 1
+			num0, num1 = num0+1, min(num0, num1)
 		} else {
-			if flag == 1 {
-				num++
-			}
+			num1 = min(num0+1, num1+1)
 		}
+		fmt.Println(num0, num1)
 	}
-	return num
+	return min(num0, num1)
 }
 
 func main() {
