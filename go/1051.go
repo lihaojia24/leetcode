3e136d8781be7c9e5784df5b52f4d3df220f9d1b
Hao Jia Li <lihaojia@vip.qq.com>
2022-06-13 11:15:47 +0800

    Create 1051.go

---
+++ go/1051.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func heightChecker(heights []int) (ans int) {
+	m := [101]int{}
+	for _, v := range heights {
+		m[v]++
+	}
+	index := 0
+	for num, count := range m {
+		for i := 0; i < count; i++ {
+			if heights[index] != num {
+				ans++
+			}
+			index++
+		}
+	}
+	return
+}
+
+func main() {
+	heights := []int{1, 1, 4, 2, 1, 3}
+	fmt.Printf("heightChecker(heights): %v\n", heightChecker(heights))
+}
