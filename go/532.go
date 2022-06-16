3826c738339e6570cc5b38b5e8769ab4c2c9ebc3
Hao Jia Li <lihaojia@vip.qq.com>
2022-06-16 09:19:39 +0800

    Create 532.go

---
+++ go/532.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func findPairs(nums []int, k int) (ans int) {
+	m := map[int]int{}
+	for _, num := range nums {
+		if k == 0 {
+			if m[num] == 1 {
+				ans++
+			}
+		} else {
+			if m[num] == 0 {
+				if m[num-k] > 0 {
+					ans++
+				}
+				if m[num+k] > 0 {
+					ans++
+				}
+			}
+		}
+		m[num]++
+	}
+	return
+}
+
+func main() {
+	nums := []int{3, 1, 4, 1, 5}
+	k := 2
+	fmt.Printf("findPairs(nums, k): %v\n", findPairs(nums, k))
+}
