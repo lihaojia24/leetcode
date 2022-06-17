9a5fb4eb50fd35c44eb7d91ee26aad1cb8ee6b1b
Hao Jia Li <lihaojia@vip.qq.com>
2022-06-17 09:38:49 +0800

    Create 1089.go

---
+++ go/1089.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func duplicateZeros(arr []int) {
+	i, j := 0, -1
+	n := len(arr)
+	for index, num := range arr {
+		j++
+		if num == 0 {
+			j++
+		}
+		if j == n-1 {
+			i = index
+			break
+		}
+		if j > n-1 {
+			i = index - 1
+			j--
+			arr[j] = 0
+			j--
+			break
+		}
+	}
+	for index := i; index >= 0; index-- {
+		arr[j] = arr[index]
+		j--
+		if arr[index] == 0 {
+			arr[j] = arr[index]
+			j--
+		}
+	}
+	return
+}
+
+func main() {
+	arr := []int{1, 2, 3}
+	duplicateZeros(arr)
+	fmt.Printf("arr: %v\n", arr)
+}
