9ed4bb388b3b4298df2fa3053ee55901e632dbec
Hao Jia Li <lihaojia@vip.qq.com>
2022-06-19 11:14:48 +0800

    Create 508.go

---
+++ go/508.go
@@ -0,0 +1,38 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findFrequentTreeSum(root *TreeNode) (ans []int) {
+	m := map[int]int{}
+	var sum func(*TreeNode) int
+	sum = func(node *TreeNode) int {
+		ans := node.Val
+		if node.Left != nil {
+			ans += sum(node.Left)
+		}
+		if node.Right != nil {
+			ans += sum(node.Right)
+		}
+		m[ans] += 1
+		return ans
+	}
+	sum(root)
+	maxV := 0
+	for k, v := range m {
+		if v > maxV {
+			maxV = v
+			ans = []int{k}
+		} else if v == maxV {
+			ans = append(ans, k)
+		}
+	}
+	return
+}
+
+func main() {
+
+}
