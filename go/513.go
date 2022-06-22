71a7801838aac894978046a7276ba3c6f6360288
Hao Jia Li <lihaojia@vip.qq.com>
2022-06-22 12:51:54 +0800

    Create 513.go

---
+++ go/513.go
@@ -0,0 +1,27 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findBottomLeftValue(root *TreeNode) int {
+	q := []*TreeNode{root}
+	var node *TreeNode
+	for len(q) > 0 {
+		node = q[0]
+		q = q[1:]
+		if node.Right != nil {
+			q = append(q, node.Right)
+		}
+		if node.Left != nil {
+			q = append(q, node.Left)
+		}
+	}
+	return node.Val
+}
+
+func main() {
+
+}
