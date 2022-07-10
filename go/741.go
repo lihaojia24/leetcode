40a8913024fdb3c304aab1e90a185b5b44d1bc8c
Hao Jia Li <lihaojia@vip.qq.com>
2022-07-10 12:08:25 +0800

    Update 741.go

--- go/741.go
+++ go/741.go
@@ -1,51 +1,58 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-var steps = [][]int{{0, 0}, {0, -1}, {-1, 0}, {-1, -1}}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
+import "math"
 
 func cherryPickup(grid [][]int) int {
 	n := len(grid)
-	dp := make([][]int, n)
-	for i := 0; i < n; i++ {
-		dp[i] = make([]int, n)
-		for j := 0; j < n; j++ {
-			dp[i][j] = math.MinInt
+	f := make([][]int, n)
+	for i := range f {
+		f[i] = make([]int, n)
+		for j := range f[i] {
+			f[i][j] = math.MinInt32
 		}
 	}
-	stepN := 2 * (n - 1)
-	dp[0][0] = grid[0][0]
-	for k := 1; k <= stepN; k++ {
-		for x1 := max(0, k-n+1); x1 <= k && x1 < n; x1++ {
-			for x2 := max(0, k-n+1); x2 <= k && x2 < n; x2++ {
+	f[0][0] = grid[0][0]
+	for k := 1; k < n*2-1; k++ {
+		for x1 := min(k, n-1); x1 >= max(k-n+1, 0); x1-- {
+			for x2 := min(k, n-1); x2 >= x1; x2-- {
 				y1, y2 := k-x1, k-x2
 				if grid[x1][y1] == -1 || grid[x2][y2] == -1 {
+					f[x1][x2] = math.MinInt32
 					continue
 				}
-				for _, step := range steps {
-					if x1+step[0] >= 0 && x2+step[1] >= 0 {
-						dp[x1][x2] = max(dp[x1][x2], dp[x1+step[0]][x2+step[1]])
-					}
+				res := f[x1][x2] // 都往右
+				if x1 > 0 {
+					res = max(res, f[x1-1][x2]) // 往下，往右
 				}
-				dp[x1][x2] += grid[x1][y1]
-				if x2 != x1 {
-					dp[x1][x2] += grid[x2][y2]
+				if x2 > 0 {
+					res = max(res, f[x1][x2-1]) // 往右，往下
 				}
+				if x1 > 0 && x2 > 0 {
+					res = max(res, f[x1-1][x2-1]) // 都往下
+				}
+				res += grid[x1][y1]
+				if x2 != x1 { // 避免重复摘同一个樱桃
+					res += grid[x2][y2]
+				}
+				f[x1][x2] = res
 			}
 		}
 	}
-	fmt.Printf("dp: %v\n", dp)
-	return max(dp[n-1][n-1], 0)
+	return max(f[n-1][n-1], 0)
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
 }
 
 func main() {
