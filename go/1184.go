8a2d31bb218e231b20a147a7a67cb9e50615aa53
Hao Jia Li <lihaojia@vip.qq.com>
2022-07-25 09:56:23 +0800

    update

---
+++ go/1184.go
@@ -0,0 +1,25 @@
+package main
+
+func distanceBetweenBusStops(distance []int, start int, destination int) int {
+	if start > destination {
+		start, destination = destination, start
+	}
+	ans1, ans2 := 0, 0
+	n := len(distance)
+	for i := 0; i < n; i++ {
+		if start <= i && i < destination {
+			ans1 += distance[i]
+		} else {
+			ans2 += distance[i]
+		}
+	}
+	if ans1 < ans2 {
+		return ans1
+	} else {
+		return ans2
+	}
+}
+
+func main() {
+
+}
---
+++ go/919.go
@@ -0,0 +1,63 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type CBTInserter struct {
+	root   *TreeNode
+	next   []*TreeNode
+	isLeft bool
+}
+
+func Constructor(root *TreeNode) CBTInserter {
+	tree := CBTInserter{root, make([]*TreeNode, 0), false}
+	q := []*TreeNode{root}
+	for len(q) > 0 {
+		node := q[0]
+		if node.Left != nil && node.Right != nil {
+			q = append(q, node.Left)
+			q = append(q, node.Right)
+			q = q[1:]
+		} else {
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			tree.next = q
+			tree.isLeft = node.Left == nil
+			break
+		}
+	}
+	return tree
+}
+
+func (this *CBTInserter) Insert(val int) int {
+	node := this.next[0]
+	newNode := &TreeNode{Val: val}
+	if this.isLeft {
+		node.Left = newNode
+	} else {
+		node.Right = newNode
+		this.next = this.next[1:]
+	}
+	this.next = append(this.next, newNode)
+	this.isLeft = !this.isLeft
+	return node.Val
+}
+
+func (this *CBTInserter) Get_root() *TreeNode {
+	return this.root
+}
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * obj := Constructor(root);
+ * param_1 := obj.Insert(val);
+ * param_2 := obj.Get_root();
+ */
+
+func main() {
+
+}
