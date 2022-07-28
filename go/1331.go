adf004ad6c14bcc65ada329db863e03d02006ba5
Hao Jia Li <lihaojia@vip.qq.com>
2022-07-28 10:18:10 +0800

    Create 1331.go

---
+++ go/1331.go
@@ -0,0 +1,25 @@
+package main
+
+import "sort"
+
+func arrayRankTransform(arr []int) []int {
+	tArr := append([]int{}, arr...)
+	sort.Ints(tArr)
+	ranks := map[int]int{}
+	rank := 1
+	for _, v := range tArr {
+		if _, ok := ranks[v]; !ok {
+			ranks[v] = rank
+			rank++
+		}
+	}
+	ans := make([]int, len(arr))
+	for i, v := range arr {
+		ans[i] = ranks[v]
+	}
+	return ans
+}
+
+func main() {
+
+}
