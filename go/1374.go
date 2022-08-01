cae4e86321cd9b3e3623a163bffbb7296b1cef0f
Hao Jia Li <lihaojia@vip.qq.com>
2022-08-01 10:28:58 +0800

    Create 1374.go

---
+++ go/1374.go
@@ -0,0 +1,17 @@
+package main
+
+import "strings"
+
+func generateTheString(n int) string {
+	ans := strings.Repeat("a", n-1)
+	if n%2 == 1 {
+		ans = ans + "a"
+	} else {
+		ans = ans + "b"
+	}
+	return ans
+}
+
+func main() {
+
+}
