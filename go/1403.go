b91094c98cdfa0818852f1e49fd2d48318f88d04
Hao Jia Li <lihaojia@vip.qq.com>
2022-08-04 08:22:01 +0800

    update

---
+++ go/1403.go
@@ -0,0 +1,27 @@
+package main
+
+import "sort"
+
+func minSubsequence(nums []int) []int {
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] > nums[j]
+	})
+	sum := 0
+	for _, v := range nums {
+		sum += v
+	}
+	sum /= 2
+	ans := []int{}
+	for _, v := range nums {
+		sum -= v
+		ans = append(ans, v)
+		if sum < 0 {
+			break
+		}
+	}
+	return ans
+}
+
+func main() {
+
+}
---
+++ go/622.go
@@ -0,0 +1,80 @@
+package main
+
+type MyCircularQueue struct {
+	nums  []int
+	start int
+	next  int
+	full  bool
+}
+
+func Constructor(k int) MyCircularQueue {
+	return MyCircularQueue{nums: make([]int, k)}
+}
+
+func (this *MyCircularQueue) EnQueue(value int) bool {
+	if this.start == this.next && this.full {
+		return false
+	}
+	this.nums[this.next] = value
+	this.next++
+	this.next %= len(this.nums)
+	if this.start == this.next {
+		this.full = true
+	}
+	return true
+}
+
+func (this *MyCircularQueue) DeQueue() bool {
+	if this.start == this.next && !this.full {
+		return false
+	}
+	this.start++
+	this.start %= len(this.nums)
+	if this.start == this.next {
+		this.full = false
+	}
+	return true
+}
+
+func (this *MyCircularQueue) Front() int {
+	if this.start == this.next && !this.full {
+		return -1
+	}
+	return this.nums[this.start]
+}
+
+func (this *MyCircularQueue) Rear() int {
+	if this.start == this.next && !this.full {
+		return -1
+	}
+	return this.nums[(this.next-1+len(this.nums))%len(this.nums)]
+}
+
+func (this *MyCircularQueue) IsEmpty() bool {
+	if this.start == this.next && !this.full {
+		return true
+	}
+	return false
+}
+
+func (this *MyCircularQueue) IsFull() bool {
+	if this.start == this.next && this.full {
+		return true
+	}
+	return false
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * obj := Constructor(k);
+ * param_1 := obj.EnQueue(value);
+ * param_2 := obj.DeQueue();
+ * param_3 := obj.Front();
+ * param_4 := obj.Rear();
+ * param_5 := obj.IsEmpty();
+ * param_6 := obj.IsFull();
+ */
+
+func main() {
+
+}
