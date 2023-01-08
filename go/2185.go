b43157b810bfa5fb3f6733468abce5232b69dffb
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-01-08 20:44:53 +0800

    Create 2185.go

---
+++ go/2185.go
@@ -0,0 +1,29 @@
+package main
+
+func prefixCount(words []string, pref string) (ans int) {
+
+	var match func(word string) bool
+	match = func(word string) bool {
+		index := 0
+		if len(word) < len(pref) {
+			return false
+		}
+		for index < len(pref) {
+			if pref[index] != word[index] {
+				return false
+			}
+			index++
+		}
+		return true
+	}
+	for _, word := range words {
+		if match(word) {
+			ans++
+		}
+	}
+	return
+}
+
+func main() {
+
+}
