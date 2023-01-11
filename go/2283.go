4ad738d5bf8f1333cadc2657e7cf73a841de019c
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-01-11 20:41:55 +0800

    Create 2283.go

---
+++ go/2283.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := "0122"
+	fmt.Printf("digitCount(nums): %v\n", digitCount(nums))
+}
+
+func digitCount(nums string) bool {
+	length := len(nums)
+	counts := make([]int, length)
+	for i, ch := range nums {
+		num := int(ch - '0')
+		counts[num]--
+		counts[i] += num
+	}
+	for _, c := range counts {
+		if c != 0 {
+			return false
+		}
+	}
+	return true
+}
