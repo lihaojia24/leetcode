35f243f938344c36a8525f78d82e79b4d949e353
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-01-13 19:47:38 +0800

    Create 2287.go

---
+++ go/2287.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "ilovecodingonleetcode"
+	target := "code"
+	fmt.Printf("rearrangeCharacters(s, target): %v\n", rearrangeCharacters(s, target))
+}
+
+func rearrangeCharacters(s string, target string) int {
+	m := map[rune]int{}
+	m2 := map[rune]int{}
+	for _, ch := range target {
+		m[ch] += 1
+	}
+
+	for _, ch := range s {
+		if _, ok := m[ch]; ok {
+			m2[ch] += 1
+		}
+	}
+	fmt.Printf("m: %v\n", m)
+	fmt.Printf("m2: %v\n", m2)
+	ans := -1
+	for k, v := range m {
+		if ans != -1 {
+			if m2[k]/v < ans {
+				ans = m2[k] / v
+			}
+		} else {
+			ans = m2[k] / v
+		}
+	}
+	return ans
+}
