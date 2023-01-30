e9dae673690a2d6fea179900c7d0db25e61b1e5e
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-01-30 20:47:46 +0800

    Create 1669.go

---
+++ go/1669.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeInBetween(list1 *ListNode, a int, b int, list2 *ListNode) *ListNode {
+	p1 := list1
+	p2 := list1
+	for i := 1; i <= b; i++ {
+		if i < a {
+			p1 = p1.Next
+		}
+		p2 = p2.Next
+	}
+	p2 = p2.Next
+	p1.Next = list2
+	for p1.Next != nil {
+		p1 = p1.Next
+	}
+	p1.Next = p2
+	return list1
+}
