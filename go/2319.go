56c9a02e69ba3a0a4def51a01ed2a1fdcf0e9eb0
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-01-31 21:07:20 +0800

    Create 2319.go

---
+++ go/2319.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+
+}
+
+func checkXMatrix(grid [][]int) bool {
+	l := len(grid)
+	for i, row := range grid {
+		for j, num := range row {
+			if i == j || i+j == l-1 {
+				if num == 0 {
+					return false
+				}
+			} else {
+				if num != 0 {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
