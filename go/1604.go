83d62208835ed4b53e60f708ec7086e765ae9239
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-02-07 23:05:02 +0800

    Create 1604.go

---
+++ go/1604.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func alertNames(keyName []string, keyTime []string) (ans []string) {
+	m := map[string][]int{}
+	for i, key := range keyName {
+		times := strings.Split(keyTime[i], ":")
+		hour, _ := strconv.Atoi(times[0])
+		minute, _ := strconv.Atoi(times[1])
+		m[key] = append(m[key], hour*60+minute)
+	}
+	for k, vs := range m {
+		if len(vs) > 2 {
+			sort.Ints(vs)
+			for i := 0; i < len(vs)-2; i++ {
+				if vs[i+2]-vs[i] <= 60 {
+					ans = append(ans, k)
+					break
+				}
+			}
+		}
+	}
+	sort.Strings(ans)
+	return
+}
+
+func main() {
+
+}
