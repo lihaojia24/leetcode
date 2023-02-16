aa20e9da16c74e1bb6959bf04c9954689eb28802
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-02-16 20:59:32 +0800

    Create 2341.go

---
+++ go/2341.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func numberOfPairs(nums []int) (ans []int) {
+	pairs := 0
+	remains := 0
+	m := map[int]bool{}
+	for _, num := range nums {
+		if m[num] {
+			remains--
+			pairs++
+			m[num] = false
+		} else {
+			remains++
+			m[num] = true
+		}
+	}
+	ans = append(ans, pairs)
+	ans = append(ans, remains)
+	return
+}
+
+func main() {
+	nums := []int{1, 3, 2, 1, 3, 2, 2}
+	fmt.Printf("numberOfPairs(nums): %v\n", numberOfPairs(nums))
+}
