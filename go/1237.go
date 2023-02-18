0e4979ccdf38b7176bd63b58a85f9bf03010ab1e
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-02-18 12:15:18 +0800

    Create 1237.go

---
+++ go/1237.go
@@ -0,0 +1,26 @@
+package main
+
+/**
+ * This is the declaration of customFunction API.
+ * @param  x    int
+ * @param  x    int
+ * @return 	    Returns f(x, y) for any given positive integers x and y.
+ *			    Note that f(x, y) is increasing with respect to both x and y.
+ *              i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ */
+
+func findSolution(customFunction func(int, int) int, z int) (ans [][]int) {
+	for x := 1; x <= 1000; x++ {
+		for y := 1; y <= 1000; y++ {
+			if customFunction(x, y) == z {
+				ans = append(ans, []int{x, y})
+				break
+			}
+		}
+	}
+	return
+}
+
+func main() {
+
+}
