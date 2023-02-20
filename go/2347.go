16058d8c1a7a5c94d1a75714cdd6c3754909367a
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-02-20 20:19:21 +0800

    Create 2347.go

---
+++ go/2347.go
@@ -0,0 +1,28 @@
+package main
+
+import "bytes"
+
+func bestHand(ranks []int, suits []byte) string {
+	if bytes.Count(suits, suits[:1]) == 5 {
+		return "Flush"
+	}
+	m := map[int]int{}
+	pair := false
+	for _, rank := range ranks {
+		m[rank]++
+		if m[rank] == 3 {
+			return "Three of a Kind"
+		}
+		if m[rank] == 2 {
+			pair = true
+		}
+	}
+	if pair {
+		return "Pair"
+	}
+	return "High Card"
+}
+
+func main() {
+
+}
