e12283533428ffa264d7dd2dac6a79e95d9bfd47
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-03-26 00:04:44 +0800

    Create 1574.py

---
+++ python/1574.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        left = 0
+        right = len(arr) - 1
+        ans = len(arr)
+        while left < right and arr[left] < arr[left+1]:
+            left += 1
+        if left == right:
+            return 0
+        while left > 0:
+            while arr[right] >= arr[left] and (right == len(arr) - 1 or (right > 0 and arr[right] < arr[right+1])):
+                right -= 1
+            print(left, right)
+            ans = min(ans, right-left+1)
+            left -= 1
+        return ans
+    
+arr = [1,2,3,10,4,2,3,5]
+s = Solution()
+print(s.findLengthOfShortestSubarray(arr))
