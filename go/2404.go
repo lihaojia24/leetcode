0055020342ccf0c5fb329ff9e10018ef347bcc13
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-04-13 19:55:17 +0800

    Create 2404.go

---
+++ go/2404.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{0, 1, 2, 2, 4, 4, 1}
+	fmt.Printf("mostFrequentEven(nums): %v\n", mostFrequentEven(nums))
+}
+
+func mostFrequentEven(nums []int) int {
+	m := map[int]int{}
+	for _, num := range nums {
+		if num%2 == 0 {
+			m[num]++
+		}
+	}
+	ans := -1
+	max := 0
+	fmt.Printf("m: %v\n", m)
+	for k, v := range m {
+		if ans == -1 || v > max || (v == max && k < ans) {
+			ans = k
+			max = v
+		}
+	}
+	return ans
+}
