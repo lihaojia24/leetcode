ccb73cfaa90caf1fe219a90e332c91e1c89d2dcd
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-05-27 19:11:35 +0800

    Create 1093.py

---
+++ python/1093.py
@@ -0,0 +1,5 @@
+
+# 作者：LeetCode-Solution
+# 链接：https://leetcode.cn/problems/statistics-from-a-large-sample/solution/da-yang-ben-tong-ji-by-leetcode-solution-xx14/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
