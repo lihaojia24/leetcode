33fc4901d5a08bdd2cb3d941ed5b1a9d2f5b442a
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-07-23 12:04:38 +0800

    Update 42.py

--- python/42.py
+++ python/42.py
@@ -3,6 +3,18 @@ from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
+        lside, rside = [0] * n, [0] * n
+        lside[0], rside[-1] = height[0], height[-1]
+        for i in range(1, n):
+            lside[i] = max(height[i], lside[i-1])
+            j = -1 * i - 1
+            rside[j] = max(height[j], rside[j+1])
+        for i in range(n):
+            lside[i] = min(lside[i], rside[i])
+        return sum([lside[i] - height[i] for i in range(n)])
+
+    def trap1(self, height: List[int]) -> int:
+        n = len(height)
         leftList, rightList = [0] * n, [0] * n
         leftList[0], rightList[-1] = height[0], height[-1]
         for i in range(1, n):
