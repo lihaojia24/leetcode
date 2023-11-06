b0ffadf8ae60d1eb62b53120b7a76c98469d5d02
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-11-06 08:51:30 +0800

    Create 318.go

---
+++ go/318.go
@@ -0,0 +1,25 @@
+package main
+
+func maxProduct(words []string) (ans int) {
+	n := len(words)
+	masks := make([]int, n)
+	for i, word := range words {
+		mask := 0
+		for _, ch := range word {
+			mask |= 1 << (ch - 'a')
+		}
+		masks[i] = mask
+	}
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			if masks[i]&masks[j] == 0 {
+				ans = max(len(words[i])*len(words[j]), ans)
+			}
+		}
+	}
+	return
+}
+
+func main() {
+
+}
